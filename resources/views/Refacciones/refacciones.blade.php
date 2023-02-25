a92e28e2aced6879cb4dcf73d320f6fedffc5e60
DosDeCarnitas1 <2122200365@soy.utj.edu.mx>
2023-02-25 00:47:24 -0600

    Arregle el cagadero de memo

--- resources/views/Refacciones/refacciones.blade.php
+++ resources/views/Refacciones/refacciones.blade.php
@@ -16,7 +16,7 @@
     
 
     <style>
-      .bd-placeholder-img {
+      /* .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
@@ -110,7 +110,7 @@
         width: 200px;
         height: 256px;
         
-      } */
+      } 
       .contact-info,
       .information,
       .my-account,
@@ -154,7 +154,7 @@
       }
 
       .social-icons {
-        /* width: 200px; */
+       width: 200px; 
         display: flex;
         gap: 1.5rem;
       }
@@ -215,7 +215,182 @@
       }
       .container_py-5{
         background: rgb(175, 106, 16);
-      }
+      } */
+
+  .footer {
+	background-color:#c7a17a;
+  /* height: 420px; */
+
+}
+
+.container-footer {
+	display: flex;
+	flex-direction: column;
+	gap: 1.5rem;
+	padding: 1rem;
+  margin-bottom: -24px;
+}
+
+.menu-footer {
+	display: grid;
+	grid-template-columns: repeat(3, 1fr) 20rem;
+	gap: 1.9rem;
+	justify-items: center;
+}
+
+.title-footer {
+	font-weight: 600;
+	font-size: 1rem;
+	text-transform: uppercase;
+}
+
+.contact-info,
+.information,
+.my-account,
+.newsletter {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+
+.contact-info ul,
+.information ul,
+.my-account ul {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+
+.contact-info ul li,
+.information ul li,
+.my-account ul li {
+	list-style: none;
+	color: #fff;
+	font-size: 1rem;
+	font-weight: 300;
+  margin-left: -33px;
+}
+
+.information ul li a,
+.my-account ul li a {
+	text-decoration: none;
+	color: #fff;
+	font-weight: 300;
+}
+
+.information ul li a:hover,
+.my-account ul li a:hover {
+	color: var(--dark-color);
+}
+
+.social-icons {
+	display: flex;
+	gap: .5rem;
+}
+
+.social-icons span {
+	border-radius: 50%;
+	width: 1.5rem;
+	height: 1.5rem;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.social-icons span i {
+	color: #fff;
+	font-size: 1rem;
+}
+
+.facebook {
+	background-color: #3b5998;
+}
+
+.twitter {
+	background-color: #00acee;
+}
+
+.youtube {
+	background-color: #c4302b;
+}
+
+.pinterest {
+	background-color: #c8232c;
+}
+
+.instagram {
+	background: linear-gradient(
+		#405de6,
+		#833ab4,
+		#c13584,
+		#e1306c,
+		#fd1d1d,
+		#f56040,
+		#fcaf45
+	);
+}
+
+.content p {
+	font-size: 1rem;
+	color: #fff;
+	font-weight: 300;
+}
+
+.content input {
+	outline: none;
+	background: none;
+	border: none;
+	border-bottom: 2px solid #d2b495;
+	cursor: pointer;
+	padding: 0.5rem 1rem;
+	color: var(--dark-color);
+	display: block;
+	margin-bottom: 1.5rem;
+	margin-top: 1.5rem;
+	width: 100%;
+	font-family: inherit;
+}
+
+.content input::-webkit-input-placeholder {
+	color: #eee;
+}
+
+.content button {
+	border: none;
+	background-color: #000;
+	color: #fff;
+	text-transform: uppercase;
+	padding: .5rem 2rem;
+	border-radius: 2rem;
+	font-size: 1rem;
+	font-family: inherit;
+	cursor: pointer;
+	font-weight: 500;
+}
+
+.content button:hover {
+	background-color: var(--background-color);
+	color: var(--primary-color);
+}
+
+.copyright {
+	display: flex;
+	justify-content: space-between;
+	padding-top: 1.5rem;
+
+	border-top: 1px solid #d2b495;
+}
+
+.img{
+  width: 234px;
+  height: 25px; 
+}
+
+.copyright p {
+	font-weight: 600;
+	font-size: 1rem;
+}
     </style>
 
     
